ba0e436a1fe24ed3897ff7591977792acceb9a18
petervu987 <petervu987@gmail.com>
2026-07-21 22:40:44 -0500

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const app = require('./src/app');
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Enable CORS
-app.use(cors());
-
-// Middleware for parsing JSON requests
-app.use(express.json());
-
-// RESTful API route for health check
-app.get('/api/health', (req, res) => {
-  res.status(200).json({ status: 'ok' });
-});
-
-// Start Express Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
